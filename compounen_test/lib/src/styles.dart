d071a871ab7f829f7920eb2383ea1d1ebf695e7c
Jose Manuel <manuelvaldez8642@gmail.com>
2020-06-20 22:25:05 -0500

    Deleate ugly Proyect

--- compounen_test/lib/src/styles.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TexStyle{
-  TextStyle titleStyle()=>TextStyle(color: Colors.black45,fontSize: 33);
-}
-
-class Background{
-  Color limeStyle()=>Colors.lime;
-}
